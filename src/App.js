a4cbba03f9d1e798f990af2538d7fc8a9e57f9c5
VirginiaRool <virginia.aro@gmail.com>
2023-06-08 17:40:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
+const sections = [
+  { title: "Section 1", content: "This is the content for section 1." },
+  { title: "Section 2", content: "This is the content for section 2." },
+  { title: "Section 3", content: "This is the content for section 3." },
+];
+
 function App() {
-  return <div className="App">Accordion</div>;
+  return (
+    <div className="App">
+      <Accordion sections={sections} />
+    </div>
+  );
 }
 
 export default App;
