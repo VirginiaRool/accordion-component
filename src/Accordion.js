59fb27e150fba8e2de2262a7d82e6e8a29f3994f
VirginiaRool <virginia.aro@gmail.com>
2023-06-09 23:27:44 -0600

    Updated Accordion.js and added css

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Accordion.css";
 
-export default function Accordion(sections) {
-  console.log({ sections });
-  return <div></div>;
+export default function Accordion({ sections }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  console.log(activeIndex);
+
+  return (
+    <div className="container">
+      {sections.map((section, index) => {
+        let contentClass = index === activeIndex ? "content active" : "content";
+        return (
+          <div className="section" key={index}>
+            <div
+              className="title"
+              onClick={() => {
+                setActiveIndex(index);
+              }}
+            >
+              {section.title}
+            </div>
+            <div className={contentClass}>
+              <p>{section.content}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
+// no voy a hacer mutipull ni hook
